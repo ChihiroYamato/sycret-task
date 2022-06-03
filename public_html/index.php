fb477dfc24675917c5aed5c54da17c42258b7dd6
Alexander Mironov <alexan9610@gmail.com>
2022-06-03 19:42:18 +0300

    Скорректирован индексный файл

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\Controllers;
+Use App\Route;
+
+Route\Web::post('/gendoc', [Controllers\ApiController::class, 'actionGenDoc']);
+
+Route\Web::close();
