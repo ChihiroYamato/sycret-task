82ee41cabfe83e30f2337ceb5627170296749e55
Alexander Mironov <alexan9610@gmail.com>
2022-06-03 19:39:55 +0300

    Add project const

---
+++ src/const.php
@@ -0,0 +1,9 @@
+<?php
+
+if (! defined('PROJECT_ROOT')) {
+    define('PROJECT_ROOT', dirname(__DIR__));
+}
+
+if (! defined('SERVER_ROOT')) {
+    define('SERVER_ROOT', PROJECT_ROOT . '/public_html');
+}
